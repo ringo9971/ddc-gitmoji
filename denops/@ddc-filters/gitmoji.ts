0ddbb44bea740545f7d04ec01d1c8167c744c9f1
ringo9971 <>
2023-05-31 22:18:18 +0900

    :bug: 動くように修正

--- denops/@ddc-filters/gitmoji.ts
+++ denops/@ddc-filters/gitmoji.ts
@@ -1,16 +1,13 @@
 import { CompletionMetadata } from "../@ddc-sources/gitmoji.ts";
-import { Candidate } from "https://deno.land/x/ddc_vim@v0.17.0/types.ts";
-import {
-  BaseFilter,
-  FilterArguments,
-} from "https://deno.land/x/ddc_vim@v0.17.0/base/filter.ts";
+import { BaseFilter, Item } from "https://deno.land/x/ddc_vim@v3.4.0/types.ts";
+import { FilterArguments } from "https://deno.land/x/ddc_vim@v3.4.0/base/filter.ts";
 
 export class Filter extends BaseFilter<Record<string, never>> {
   async filter(
     args: FilterArguments<Record<string, never>>
-  ): Promise<Candidate[]> {
-    return args.candidates.filter(candidate => {
-      const meta = candidate.user_data as unknown as CompletionMetadata;
+  ): Promise<Item[]> {
+    return args.items.filter((item) => {
+      const meta = item.user_data as unknown as CompletionMetadata;
       return meta && meta.name.startsWith(`${args.completeStr}`);
     });
   }
